6e8d9bbadf46142c918257f0584dcbc3d790fe4a
Ruud Kok <git@ruudkok.com>
2011-01-20 19:40:06 +0100

    Fixed for IIS5.1 Added Default.aspx which routes to Home/Index Added Root Route to Global.asax.cs.

---
+++ MoCS/MoCS.WebClient/default.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Mvc;
+
+namespace MoCS.WebClient
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HttpContext.Current.RewritePath(Request.ApplicationPath, false);
+            IHttpHandler httpHandler = new MvcHttpHandler();
+            httpHandler.ProcessRequest(HttpContext.Current);
+        }
+    }
+}
