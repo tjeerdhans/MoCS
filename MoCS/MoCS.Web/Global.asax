b2953dfb75d268e31b4bfc8924da66fd4acc6432
tjeerdhans <tjeerdhans@gmail.com>
2014-02-18 18:09:00 +0100

    MoCS.Web added (mvc5), solution folder renamed to web, enabled nuget package restore.

---
+++ MoCS/MoCS.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MoCS.Web.MvcApplication" Language="C#" %>
---
+++ MoCS/MoCS.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MoCS.Web.Startup))]
+namespace MoCS.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
