b2dbe7ebddea92cf277443fe600bc215b2017066
tjeerdhans <tjeerdhans@gmail.com>
2014-03-03 21:22:44 +0100

    boilerplate assignments controller added.

---
+++ MoCS/MoCS.Web/Controllers/AssignmentsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MoCS.Web.Controllers
+{
+    [HandleError]
+    [Authorize(Roles = "Admin")]
+    public class AssignmentsController : Controller
+    {
+        //
+        // GET: /Assignments/
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
