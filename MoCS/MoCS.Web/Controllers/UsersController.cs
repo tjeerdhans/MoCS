0c503a9c97fcf470d91003d6b81376fc51699eed
tjeerdhans <tjeerdhans@gmail.com>
2014-03-26 22:13:40 +0100

    Added basic users index view

--- MoCS/MoCS.Web/Controllers/UsersController.cs
+++ MoCS/MoCS.Web/Controllers/UsersController.cs
@@ -1,16 +1,27 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 using MoCS.Data.Repositories;
+using MoCS.Web.Models;
 
 namespace MoCS.Web.Controllers
 {
     public class UsersController : Controller
     {
         private readonly UnitOfWork _unitOfWork = new UnitOfWork();
+
+
         //
         // GET: /Users/
         public ActionResult Index()
         {
-            return View();
+            var model = new UserModel();
+            using (var appContext = new ApplicationDbContext())
+            {
+                model.UserModelItems = appContext.Users.ToList().Select(u => new UserModelItem(u, _unitOfWork.TeamsRepository.Single(t => t.Id == u.TeamId)));
+            }
+
+            return View(model);
         }
-	}
+    }
 }
---
+++ MoCS/MoCS.Web/Models/UserModels.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MoCS.Data.Entity;
+
+namespace MoCS.Web.Models
+{
+    public class UserModel
+    {
+        public IEnumerable<UserModelItem> UserModelItems { get; set; }
+
+        public UserModel()
+        {
+            UserModelItems = new List<UserModelItem>();
+        }
+    }
+
+    public class UserModelItem
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string TeamName { get; set; }
+
+        public UserModelItem(ApplicationUser user, Team team)
+        {
+            Id = user.Id;
+            UserName = user.UserName;
+            TeamName = team.Name;
+        }
+    }
+}
